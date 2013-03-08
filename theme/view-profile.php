379ea7b66efcda0fb0cc4e1856baa070340779cf
apswebdev <anwar_saludsong@yahoo.com>
2013-03-08 23:23:26 +0800

    Code Clean Up

--- theme/view-profile.php
+++ theme/view-profile.php
@@ -161,51 +161,49 @@ while ($data = mysql_fetch_array($results)) {
 							$rb("#"+id+" .customScrollBox .content").load(file,function(){
 								mCustomScrollbars();
 							});
-						});
-					})(jQuery);
+						}
 				</script>
-				<?
-			// Slider Gallery			
-			} elseif ($rb_agency_option_layoutprofile == "3") {
-				?>
-				<script>
-				var $tab = jQuery.noConflict();
-				$tab(window).load(function() {
-						/*$tab(".tab").hide();
-						$tab(".row-photos").show();
-						$tab(".tab-active").removeClass("tab-active").addClass("tab-inactive");
-						$tab("#row-photos").removeClass("tab-inactive").addClass("tab-active");*/
-						$tab(".maintab").click(function(){
-							   var idx = this.id;
-							   var elem = "." + idx;
-							   var elem_id = "#" + idx;
-							   if ((idx=="row-all")){
-										$tab(".tab").hide();
-										$tab(".tab").show().css({ opacity: 0.0 }).stop().animate({ opacity: 1.0 }, 2000);
-										$tab(".tab-active").removeClass("tab-active").addClass("tab-inactive");
-								} else {
-									  if(idx=="row-bookings"){
-									  		
-											var url = "<?php echo get_permalink(get_page_by_title('booking')); ?>";
-											window.location = url;
-											
-									  } else {
-											   
-											  $tab(".tab-active").removeClass("tab-active").addClass("tab-inactive");
-											  $tab(".tab").css({ opacity: 1.0 }).stop().animate({ opacity: 0.0 }, 2000).hide();
-											  $tab(elem).show().css({ opacity: 0.0 }).stop().animate({ opacity: 1.0 }, 2000);
-											  $tab(elem_id).removeClass("tab-inactive").addClass("tab-active");
-									  }
-							   }
+						<?
+					// Slider Gallery			
+					} elseif ($rb_agency_option_layoutprofile == "3") {
+						?>
+						<script>
+						var $tab = jQuery.noConflict();
+						$tab(window).load(function() {
+								/*$tab(".tab").hide();
+								$tab(".row-photos").show();
+								$tab(".tab-active").removeClass("tab-active").addClass("tab-inactive");
+								$tab("#row-photos").removeClass("tab-inactive").addClass("tab-active");*/
+								$tab(".maintab").click(function(){
+									   var idx = this.id;
+									   var elem = "." + idx;
+									   var elem_id = "#" + idx;
+									   if ((idx=="row-all")){
+												$tab(".tab").hide();
+												$tab(".tab").show().css({ opacity: 0.0 }).stop().animate({ opacity: 1.0 }, 2000);
+												$tab(".tab-active").removeClass("tab-active").addClass("tab-inactive");
+										} else {
+											  if(idx=="row-bookings"){
+													
+													var url = "<?php echo get_permalink(get_page_by_title('booking')); ?>";
+													window.location = url;
+													
+											  } else {
+													   
+													  $tab(".tab-active").removeClass("tab-active").addClass("tab-inactive");
+													  $tab(".tab").css({ opacity: 1.0 }).stop().animate({ opacity: 0.0 }, 2000).hide();
+													  $tab(elem).show().css({ opacity: 0.0 }).stop().animate({ opacity: 1.0 }, 2000);
+													  $tab(elem_id).removeClass("tab-inactive").addClass("tab-active");
+											  }
+									   }
+								});
 						});
-				});
-				</script>
-				<?php
-			}
-			
-		}
-}
-}
+						</script>
+						<?php
+					}//
+		        }
+          } // function end
+    }// if function exist(rb_agency_inserthead_profile)
    
 // GET HEADER  
 	get_header();
