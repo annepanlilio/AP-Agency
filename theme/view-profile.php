26bd0b940d409525f58ffa0b9bfd8c0dc88ca13a
apswebdev <anwar_saludsong@yahoo.com>
2013-03-08 23:15:06 +0800

    This commit is a fix for "all tab" glitch and added a direct link for booking page on booking tab

--- theme/view-profile.php
+++ theme/view-profile.php
@@ -161,47 +161,50 @@ while ($data = mysql_fetch_array($results)) {
 							$rb("#"+id+" .customScrollBox .content").load(file,function(){
 								mCustomScrollbars();
 							});
-						}
-						</script>
-						<?
-					// Slider Gallery			
-					} elseif ($layouttype == 3) {
-						?>
-						<script>
-						var $tab = jQuery.noConflict();
-						$tab(window).load(function() {
-								/*$tab(".tab").hide();
-								$tab(".row-photos").show();
-								$tab(".tab-active").removeClass("tab-active").addClass("tab-inactive");
-								$tab("#row-photos").removeClass("tab-inactive").addClass("tab-active");*/
-								$tab(".maintab").click(function(){
-									   var idx = this.id;
-									   var elem = "." + idx;
-									   var elem_id = "#" + idx;
-									   if ((idx=="row-all") || (idx=="row-booking")){
-												$tab(".tab").hide("slow");
-												$tab(".tab").show().css({ opacity: 0.0 }).stop().animate({ opacity: 1.0 }, 2000);
-												$tab(".tab-active").removeClass("tab-active").addClass("tab-inactive");
-												$tab(elem_id).removeClass("tab-inactive").addClass("tab-active");
-										} else {
+						});
+					})(jQuery);
+				</script>
+				<?
+			// Slider Gallery			
+			} elseif ($rb_agency_option_layoutprofile == "3") {
+				?>
+				<script>
+				var $tab = jQuery.noConflict();
+				$tab(window).load(function() {
+						/*$tab(".tab").hide();
+						$tab(".row-photos").show();
+						$tab(".tab-active").removeClass("tab-active").addClass("tab-inactive");
+						$tab("#row-photos").removeClass("tab-inactive").addClass("tab-active");*/
+						$tab(".maintab").click(function(){
+							   var idx = this.id;
+							   var elem = "." + idx;
+							   var elem_id = "#" + idx;
+							   if ((idx=="row-all")){
+										$tab(".tab").hide();
+										$tab(".tab").show().css({ opacity: 0.0 }).stop().animate({ opacity: 1.0 }, 2000);
+										$tab(".tab-active").removeClass("tab-active").addClass("tab-inactive");
+								} else {
+									  if(idx=="row-bookings"){
+									  		
+											var url = "<?php echo get_permalink(get_page_by_title('booking')); ?>";
+											window.location = url;
+											
+									  } else {
+											   
 											  $tab(".tab-active").removeClass("tab-active").addClass("tab-inactive");
 											  $tab(".tab").css({ opacity: 1.0 }).stop().animate({ opacity: 0.0 }, 2000).hide();
 											  $tab(elem).show().css({ opacity: 0.0 }).stop().animate({ opacity: 1.0 }, 2000);
 											  $tab(elem_id).removeClass("tab-inactive").addClass("tab-active");
-									   }
-								});
+									  }
+							   }
 						});
-						</script>
-						<?php
-					
-					} elseif ($layouttype == 9) {
-
-					}
-				}
-
-				
+				});
+				</script>
+				<?php
 			}
-	}
+			
+		}
+}
 }
    
 // GET HEADER  
