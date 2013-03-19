99e94749acc1faea0be9f7adcf5ac7235c98efb8
andrei-sokolov <andreisokolov28>
2013-03-19 21:31:31 +0400

    more changes on generating login/password feature (registering users, generating login/pass for multiple users, etc...)

--- admin/profile_list_table.php
+++ admin/profile_list_table.php
@@ -1,13 +1,16 @@
+<a class="button-secondary" id="bulk_generate" href="javascript:void(0)" style="margin-bottom: 5px" title="Generate" disabled="disabled">Generate</a>
+<a class="button-primary"  id="bulk_send_email" href="javascript:void(0)" style="margin-left: 5px" title="Send Email" disabled="disabled">Send Email</a>
 <table cellspacing="0" class="widefat fixed">
  <thead>
     <tr class="thead">
+        <th class="manage-column column-cb check-column" id="cb" scope="col"><input type="checkbox"/></th>
         <th style="width:50px;"><a href="<?php echo admin_url("admin.php?page=". $_GET['page'] ."&ConfigID=99&sort=ProfileID&dir=". $sortDirection) ?>">ID</a></th>
         <th style="width:250px;"><a href="<?php echo admin_url("admin.php?page=". $_GET['page'] ."&ConfigID=99&sort=ProfileContactNameFirst&dir=". $sortDirection) ?>">First Name</a></th>
         <th style="width:250px;"><a href="<?php echo admin_url("admin.php?page=". $_GET['page'] ."&ConfigID=99&sort=ProfileContactNameLast&dir=". $sortDirection) ?>">Last Name</a></th>
         <th style="width:250px;"><a href="<?php echo admin_url("admin.php?page=". $_GET['page'] ."&ConfigID=99&sort=ProfileGender&dir=". $sortDirection) ?>">Email Addresses</a></th>
-        <th style="width:100px;">#</th>
-        <th style="width:100px;">#</th>
-        <th>#</th>
+        <th style="width:100px;"></th>
+        <th style="width:100px;"></th>
+        <th></th>
     </tr>
  </thead>
  <tbody>
@@ -33,13 +36,19 @@ while ($data = mysql_fetch_array($results2)) {
 
 ?>
     <tr <?php echo $rowColor ?>>
+        <th class="check-column" scope="row"><input type="checkbox" value="<?php echo $ProfileID ?>" id="<?php echo $ProfileID ?>" data-firstname="<?php echo $ProfileContactNameFirst ?>" data-lastname="<?php echo $ProfileContactNameLast ?>" data-email="<?php echo $ProfileContactEmail ?>" class="administrator"  name="<?php echo $ProfileID ?>"/></th>
         <td><?php echo $ProfileID ?></td>
         <td><?php echo $ProfileContactNameFirst ?></td>
         <td><?php echo $ProfileContactNameLast ?></td>
         <td><?php echo $ProfileContactEmail ?></td>
         <td><a href="javascript:void(0)" class="generate_lp button-secondary" data-id="<?php echo $ProfileID ?>" data-firstname="<?php echo $ProfileContactNameFirst ?>" data-lastname="<?php echo $ProfileContactNameLast ?>">Generate</a></td>
-        <td><a href="#" class="email_lp button-primary" data-id="<?php echo $ProfileID ?>" data-email="<?php echo $ProfileContactEmail ?>">Send Email</a></td>
-        <td><input id="l_<?php echo $ProfileID ?>" type="text" placeholder="Login" /><input id="p_<?php echo $ProfileID ?>" type="text" placeholder="Password" /></td>
+        <td><a href="javascript:void(0)" class="email_lp button-primary" disabled="disabled" data-id="<?php echo $ProfileID ?>" id="em_<?php echo $ProfileID ?>" data-email="<?php echo $ProfileContactEmail ?>">Send Email</a></td>
+        <td>
+            <div id="ch_<?php echo $ProfileID ?>"></div>
+            <input id="l_<?php echo $ProfileID ?>" type="text" placeholder="Login" /><br />
+            <input id="p_<?php echo $ProfileID ?>" type="text" placeholder="Password" />
+           
+        </td>
 
     </tr>
 <?php
@@ -74,13 +83,14 @@ while ($data = mysql_fetch_array($results2)) {
  </tbody>
   <tfoot>
     <tr class="thead">
+        <th class="manage-column column-cb check-column" id="cb" scope="col"><input type="checkbox" /></th>
         <th class="column" scope="col">ID</th>
         <th class="column" scope="col">First Name</th>
         <th class="column" scope="col">Last Name</th>
         <th class="column" scope="col">Email Address</th>
-        <th class="column" scope="col">#</th>
-        <th class="column" scope="col">#</th>
-        <th class="column" scope="col">#</th>
+        <th class="column" scope="col"></th>
+        <th class="column" scope="col"></th>
+        <th class="column" scope="col"></th>
     </tr>
  </tfoot>
 </table>
@@ -92,42 +102,121 @@ jQuery(document).ready(function($){
         var pfname = $(this).attr('data-firstname');
         var plname = $(this).attr('data-lastname');
         
-        var login = pfname.toLowerCase().substr(0, 5).replace(' ', '-') + pid + plname.toLowerCase().substr(-3, 3);
-        var password = generatepass(login);
+        var lp_arr = generateLP(pid, pfname, plname);
+
+        var login = lp_arr[0];
+        var password = lp_arr[1];
         
         $('#l_' + pid).val(login);
         $('#p_' + pid).val(password);
+        $('#em_' + pid).removeAttr('disabled');
+        $('#em_' + pid).bind('click', sendEmail);
+        $('#ch_' + pid).addClass('pending-profile');
    });
    
-   $('.email_lp').click(function(){
-       var pid = $(this).attr('data-id');
+   
+   $('#cb input[type=checkbox], .administrator').click(function(){ 
+       if($(this).is(':checked')){
+           $('#bulk_generate').removeAttr('disabled');
+           $('#bulk_generate').unbind('click').bind('click', bulkGenerateLP);
+       }
+       else { 
+           if($('.administrator:checked').length == 0 || $('.administrator:checked').length == 50)
+                $('#bulk_generate').attr('disabled', 'disabled');
+       }
+   });
+   
+   
+   function generateLP(pid, pfname, plname){
+       var login = pfname.toLowerCase().substr(0, 5).replace(' ', '-') + pid + plname.toLowerCase().substr(-3, 3);
+       var password = generatepass(login);
        
-       var login = $('#l_' + pid).val();
-       var password = $('#p_' + pid).val();
-       var email = $(this).attr('data-email').toLowerCase();
+       return [login, password];
+   }
+   
+   function bulkGenerateLP(){
+       if($('.administrator:checked').length > 0){ 
+           $('.administrator:checked').each(function(){
+                var pid = $(this).attr('id');
+                var pfname = $(this).attr('data-firstname');
+                var plname = $(this).attr('data-lastname');
 
-       if(login && password && email){
-           $.ajax({
-               url: 'admin.php?page=rb_agency_menu_reports&ConfigID=99&action=send_mail',
-               type: 'post',
-               data: {
+                var lp_arr = generateLP(pid, pfname, plname);
+
+                var login = lp_arr[0];
+                var password = lp_arr[1];
+
+                $('#l_' + pid).val(login);
+                $('#p_' + pid).val(password);
+                $('#em_' + pid).removeAttr('disabled');
+                $('#em_' + pid).bind('click', sendEmail);
+                $('#ch_' + pid).addClass('pending-profile');
+           });
+           
+           $('#bulk_send_email').removeAttr('disabled');
+           $('#bulk_send_email').bind('click', bulkSendEmail);
+       }
+   }
+   
+   function sendEmail(){
+           var pid = $(this).attr('data-id');
+
+           var login = $('#l_' + pid).val();
+           var password = $('#p_' + pid).val();
+           var email = $(this).attr('data-email').toLowerCase();
+
+           if(login && password && email){
+               $('#ch_' + pid).removeClass('pending-profile').addClass('loading-profile');
+               $.ajax({
+                   url: 'admin.php?page=rb_agency_menu_reports&ConfigID=99&action=send_mail',
+                   type: 'post',
+                   data: {
+                       profileid : pid,
+                       login : login,
+                       password : password,
+                       email : email
+                   },
+                   success: function(){
+                       //alert('Email sent successfully');
+                       $('#ch_' + pid).removeClass('loading-profile').addClass('checked-profile');
+                       $('#l_' + pid).attr('disabled', 'disabled');
+                       $('#p_' + pid).attr('disabled', 'disabled');
+                       $('#em_' + pid).attr('disabled', 'disabled');
+                       $('#em_' + pid).unbind('click');
+                   }
+               });
+           }
+           else {
+               alert("Please Generate Login / Password, then send!");
+               return false;
+           }
+    }
+    
+    function bulkSendEmail(){
+        var usersLP = [];
+        if($('.administrator:checked').length > 0){ 
+           $('.administrator:checked').each(function(){
+               var pid = $(this).attr('id');
+               var login = $('#l_' + pid).val();
+               var password = $('#p_' + pid).val();
+               var email = $(this).attr('data-email');
+               
+               usersLP[pid] = {
+                   pid : pid,
                    login : login,
                    password : password,
                    email : email
-               },
-               success: function(){
-                   alert('Email sent successfully');
-               }
+               };
+               
            });
-       }
-       else {
-           alert("Please Generate Login / Password, then send!");
-           return false;
-       }
-       
-   });
+           
+           console.log(usersLP);
+        }
+    }
    
 });
+
+
 
 var keylist = "abcdefghijklmnopqrstuvwxyz123456789";
 
