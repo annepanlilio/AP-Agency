1f1052a7bdb8b4dff7a3400959040e1a19ba090f
apswebdev <anwar_saludsong@yahoo.com>
2013-08-21 14:08:02 +0800

    Just a basic code indentation

--- theme/include-profile-search.php
+++ theme/include-profile-search.php
@@ -37,10 +37,10 @@ $rb_agency_options_arr = get_option('rb_agency_options');
 		}
 	} 
 ?>
-<!-- RESET BACKUP -->
+        <!-- RESET BACKUP -->
 	<style>
-#rst_btn { padding: 6px 10px; padding: 0.428571429rem 0.714285714rem; font-size: 13px; line-height: 1.428571429; font-weight: bold; color: #fff; background-color: #333; background-repeat: repeat-x; background-image: -moz-linear-gradient(top, #666, #333); background-image: -ms-linear-gradient(top, #666, #333); background-image: -webkit-linear-gradient(top, #666, #333); background-image: -o-linear-gradient(top, #666, #333); background-image: linear-gradient(top, #666, #333); border: none; border-radius: 3px; box-shadow: 0 1px 2px rgba(64, 64, 64, 0.1); }
-</style>
+	#rst_btn { padding: 6px 10px; padding: 0.428571429rem 0.714285714rem; font-size: 13px; line-height: 1.428571429; font-weight: bold; color: #fff; background-color: #333; background-repeat: repeat-x; background-image: -moz-linear-gradient(top, #666, #333); background-image: -ms-linear-gradient(top, #666, #333); background-image: -webkit-linear-gradient(top, #666, #333); background-image: -o-linear-gradient(top, #666, #333); background-image: linear-gradient(top, #666, #333); border: none; border-radius: 3px; box-shadow: 0 1px 2px rgba(64, 64, 64, 0.1); }
+	</style>
 	<script type="text/javascript">
 	jQuery(document).ready(function(){
 		jQuery.fn.rset = function(slect){
@@ -75,11 +75,11 @@ $rb_agency_options_arr = get_option('rb_agency_options');
 		echo "        		<div><input type=\"hidden\" name=\"action\" value=\"search\" /></div>\n";
 		echo "	 			      <div class=\"search-field single\">\n";
 		echo "		 				<label for=\"ProfileFirstName\">". __("First Name", rb_agency_TEXTDOMAIN) ."</label>\n";
-	    echo "		 				<input type=\"text\" id=\"ProfileContactNameFirst\" name=\"ProfileContactNameFirst\" value=\"". $_SESSION["ProfileContactNameFirst"] ."\" />\n";
-	    echo "	 				</div>\n";
+		echo "		 				<input type=\"text\" id=\"ProfileContactNameFirst\" name=\"ProfileContactNameFirst\" value=\"". $_SESSION["ProfileContactNameFirst"] ."\" />\n";
+		echo "	 				</div>\n";
 		echo "	 				<div class=\"search-field single\">\n";
 		echo "		 				<label for=\"ProfileLastName\">". __("Last Name", rb_agency_TEXTDOMAIN) ."</label>\n";
-	    echo "						 <input type=\"text\" id=\"ProfileContactNameLast\" name=\"ProfileContactNameLast\" value=\"". $_SESSION["ProfileContactNameLast"] ."\" />\n";
+		echo "						 <input type=\"text\" id=\"ProfileContactNameLast\" name=\"ProfileContactNameLast\" value=\"". $_SESSION["ProfileContactNameLast"] ."\" />\n";
 		echo "					 </div>\n";		
 		echo "				    <div class=\"search-field single\">\n";
 		echo "				       <label for=\"ProfileType\">". __("Type", rb_agency_TEXTDOMAIN) . "</label>\n";
@@ -104,7 +104,7 @@ $rb_agency_options_arr = get_option('rb_agency_options');
 											while ($dataGender = mysql_fetch_array($results2)) {
 												echo "<option value=\"". $dataGender["GenderID"] ."\"".selected($_SESSION['ProfileGender'],$dataGender["GenderID"],false).">". $dataGender["GenderTitle"] ."</option>";
 											}
-	    echo "				        </select>\n";
+	        echo "				        </select>\n";
 		echo "				    </div>\n";
 		echo "				    <fieldset class=\"search-field multi\">";
 		echo "				        <legend>". __("Age", rb_agency_TEXTDOMAIN) . "</legend>";
@@ -125,7 +125,7 @@ $rb_agency_options_arr = get_option('rb_agency_options');
 		echo "		</div>\n";
 		break;
  
-   /* ----------------------------------------------------------------------
+       /* ----------------------------------------------------------------------
  	*	ADVANCE SEARCH ONLY IN CONTENT
  	* ---------------------------------------------------------------------- */	
   	case "ADVANCE-LAYOUT":
@@ -135,11 +135,11 @@ $rb_agency_options_arr = get_option('rb_agency_options');
 		echo "        		<input type=\"hidden\" name=\"action\" value=\"search\" />\n";
 		echo "	 			      <div class=\"search-field single\">\n";
 		echo "		 				<label for=\"ProfileFirstName\">". __("First Name", rb_agency_TEXTDOMAIN) ."</label>\n";
-	    echo "		 				<input type=\"text\" id=\"ProfileContactNameFirst\" name=\"ProfileContactNameFirst\" value=\"". $_SESSION["ProfileContactNameFirst"] ."\" />\n";
-	    echo "	 				</div>\n";
+	        echo "		 				<input type=\"text\" id=\"ProfileContactNameFirst\" name=\"ProfileContactNameFirst\" value=\"". $_SESSION["ProfileContactNameFirst"] ."\" />\n";
+	        echo "	 				</div>\n";
 		echo "	 				<div class=\"search-field single\">\n";
 		echo "		 				<label for=\"ProfileLastName\">". __("Last Name", rb_agency_TEXTDOMAIN) ."</label>\n";
-	    echo "						 <input type=\"text\" id=\"ProfileContactNameLast\" name=\"ProfileContactNameLast\" value=\"". $_SESSION["ProfileContactNameLast"] ."\" />\n";
+	        echo "						 <input type=\"text\" id=\"ProfileContactNameLast\" name=\"ProfileContactNameLast\" value=\"". $_SESSION["ProfileContactNameLast"] ."\" />\n";
 		echo "					 </div>\n";
 		echo "				    <div class=\"search-field single\">\n";
 		echo "				       <label for=\"ProfileType\">". __("Type", rb_agency_TEXTDOMAIN) . "</label>\n";
@@ -164,7 +164,7 @@ $rb_agency_options_arr = get_option('rb_agency_options');
 												} else { $selectedvalue = ""; }
 												echo "<option value=\"". $dataGender["GenderID"] ."\"".selected($_SESSION['ProfileGender'],$dataGender["GenderID"] ,false).">". $dataGender["GenderTitle"] ."</option>";
 											}
-	    echo "				        </select>\n";
+	        echo "				        </select>\n";
 		echo "				    </div>\n";
 		
 		echo "				    <fieldset class=\"search-field multi\">";
@@ -179,23 +179,23 @@ $rb_agency_options_arr = get_option('rb_agency_options');
 		include("include-custom-fields.php");
 		echo "	 			      <div class=\"search-field single\">\n";
 		echo "		 				<label for=\"ProfileCity\">". __("City", rb_agency_TEXTDOMAIN) ."</label>\n";
-	    echo "		 				<input type=\"text\" id=\"ProfileCity\" name=\"ProfileCity\" value=\"". $_SESSION["ProfileCity"] ."\" />\n";
-	    echo "	 				</div>\n";
+	        echo "		 				<input type=\"text\" id=\"ProfileCity\" name=\"ProfileCity\" value=\"". $_SESSION["ProfileCity"] ."\" />\n";
+	        echo "	 				</div>\n";
 		echo "	 			      <div class=\"search-field single\">\n";
 		echo "		 				<label for=\"ProfileState\">". __("State", rb_agency_TEXTDOMAIN) ."</label>\n";
-	    echo "		 				<input type=\"text\" id=\"ProfileState\" name=\"ProfileState\" value=\"". $_SESSION["ProfileState"] ."\" />\n";
-	    echo "	 				</div>\n";
+	        echo "		 				<input type=\"text\" id=\"ProfileState\" name=\"ProfileState\" value=\"". $_SESSION["ProfileState"] ."\" />\n";
+	        echo "	 				</div>\n";
 		echo "	 			      <div class=\"search-field single\">\n";
 		echo "		 				<label for=\"ProfileZip\">". __("Zip", rb_agency_TEXTDOMAIN) ."</label>\n";
-	    echo "		 				<input type=\"text\" id=\"ProfileZip\" name=\"ProfileZip\" value=\"". $_SESSION["ProfileZip"] ."\" />\n";
-	    echo "	 				</div>\n";
+	        echo "		 				<input type=\"text\" id=\"ProfileZip\" name=\"ProfileZip\" value=\"". $_SESSION["ProfileZip"] ."\" />\n";
+	        echo "	 				</div>\n";
 		echo "				<div><input type=\"hidden\" name=\"ProfileIsActive\" value=\"1\" /></div>\n";
 		echo "				<div class=\"search-field submit\">";
 		echo "				<input type=\"submit\" value=\"". __("Search Profiles", rb_agency_TEXTDOMAIN) . "\" class=\"button-primary\" onclick=\"this.form.action='".get_bloginfo("wpurl")."/profile-search/'\" />";
 		echo '				<input type="button" id="rst_btn" class=\"button-primary\" value="Empty Form">';
 		echo "				<input type=\"submit\" name=\"basic_search\" value=\"". __("Basic Search", rb_agency_TEXTDOMAIN) . "\" class=\"button-primary\" onclick=\"this.form.action='".get_bloginfo("wpurl")."/search'\" />";
 		echo "				</div>";
- 	    echo'				<div></div>';
+ 	        echo'				<div></div>';
 		echo "        	</form>\n";
 	    break;
 	}
