f615862fc5eea83056e221f776099f53e7df5824
apswebdev <anwar_saludsong@yahoo.com>
2013-08-21 19:50:55 +0800

    fix on front end search check boxes custom fields

--- functions.php
+++ functions.php
@@ -935,46 +935,44 @@ error_reporting(0);
                                     } elseif ($ProfileCustomType["ProfileCustomType"] == 5) { //Checkbox
                                             if(!empty($val)){
                                                 if(strpos($val,",") === false){
-                                                   // $val = implode("','",explode(",",$val));
 												  
-                                                    if($filter2==""){
-													
-                                                            $filter2 .= " AND  ((customfield_mux.ProfileCustomValue like('%".$val."%') and customfield_mux.ProfileCustomID = '".substr($key,15)."') ";
-                                                    } else {
-                                                            $filter2 .= " OR  (customfield_mux.ProfileCustomValue like('%".$val."%')   and customfield_mux.ProfileCustomID = '".substr($key,15)."') ";
-                                                    }
-                                                } else {
-													
-                                                            $likequery = explode(",", $val);
-                                                            $likecounter = count($likequery);
-                                                            $i=1; 
-                                                            $likedata = "" ;
-                                                            foreach($likequery as $like){
-                                                                    if($i < ($likecounter-1)){
-                                                                            if($like!=""){
-                                                                                    $likedata.= " customfield_mux.ProfileCustomValue like('%".$like."%')  OR "  ;
-                                                                            }
-                                                                            }else{
-                                                                            if($like!=""){
-                                                                                            $likedata.= " customfield_mux.ProfileCustomValue like('%".$like."%')  "  ;
-                                                                            } 
-                                                                    }
-                                                                    $i++;
-                                                            }
+						      if($filter2==""){
+															      
+							      $filter2 .= " AND  ((customfield_mux.ProfileCustomValue = '".$val."' AND customfield_mux.ProfileCustomID = ".substr($key,15).") ";
+						      } else {
+							      $filter2 .= " OR  (customfield_mux.ProfileCustomValue = '".$val."' AND customfield_mux.ProfileCustomID = ".substr($key,15).") ";
+						      }
+						  } else {
+															      
+							  $likequery = explode(",", $val);
+							  $likecounter = count($likequery);
+							  $i=1; 
+							  $likedata = "" ;
+							  foreach($likequery as $like){
+								  if($i < ($likecounter-1)){
+									  if($like!=""){
+										  $likedata.= " customfield_mux.ProfileCustomValue ='".$like."' OR "  ;
+									  }
+									  }else{
+									  if($like!=""){
+											  $likedata.= " customfield_mux.ProfileCustomValue ='".$like."' "  ;
+									  } 
+								  }
+								  $i++;
+							  }
+							  
+							  $val = substr($val, 0, -1);
+						      if($filter2==""){
+							  $filter2 .= " AND  (( ".$likedata." and customfield_mux.ProfileCustomID = ".substr($key,15)." )";
+						      } else {
+							  $filter2 .= " OR  (".$likedata." and customfield_mux.ProfileCustomID = ".substr($key,15).")";
+						      }
+						  }
 
-
-                                                            $val = substr($val, 0, -1);
-                                                        if($filter2==""){
-                                                        $filter2 .= " AND  ((( ".$likedata.") and customfield_mux.ProfileCustomID = '".substr($key,15)."' )";
-                                                    } else {
-                                                        $filter2 .= " OR  ((".$likedata.") and customfield_mux.ProfileCustomID = '".substr($key,15)."')";
-                                                    }
-                                                }
-
-                                            $_SESSION[$key] = $val;
-                                            }else{
-                                                    $_SESSION[$key] = "";
-                                            }
+					      $_SESSION[$key] = $val;
+					      }else{
+						      $_SESSION[$key] = "";
+					      }
                                     } elseif ($ProfileCustomType["ProfileCustomType"] == 6) { //Radiobutton 
                                             //var_dump($ProfileCustomType["ProfileCustomType"]);
                                               // $val = implode("','",explode(",",$val));
