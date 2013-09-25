2e49aaea12587245eb5608a91d1eed973a2addd0
robertholf <rob@bertholf.com>
2013-09-25 02:33:40 -1000

    Adding new admin class

---
+++ app/admin-settings.class.php
@@ -0,0 +1,132 @@
+<?php
+
+class RBAgency_AdminSettings {
+
+	/*
+	 * Overview
+	 */
+		static function Overview() {
+		?>
+		<div id="welcome-panel" class="welcome-panel">
+			<div class="welcome-panel-content">
+				<div class="welcome-panel-column-container">
+					<div class="welcome-panel-column">
+						<h3><?php echo __("Configuration", rb_agency_TEXTDOMAIN ) ?>:</h3>
+						<ul>
+							<li><a href="?page=<?php echo $_GET["page"]; ?>&ConfigID=1"><strong><?php _e("Settings", rb_agency_TEXTDOMAIN); ?></strong></a> - <?php _e("Access this area to manage all of the core settings including layout types, privacy settings and more", rb_agency_TEXTDOMAIN); ?></li>
+							<li><a href="?page=<?php echo $_GET["page"]; ?>&ConfigID=2"><strong><?php _e("Style", rb_agency_TEXTDOMAIN); ?></strong></a> - <?php _e("Access this area to manage all of the core settings including layout types, privacy settings and more", rb_agency_TEXTDOMAIN); ?></li>
+						</ul>
+					</div>
+
+					<div class="welcome-panel-column" style="margin-left: 50px;">
+						<h3><?php echo __("Diagnostics", rb_agency_TEXTDOMAIN ) ?>:</h3>
+							<?php
+							echo "No Diagnostic has been run.  Please run now.";
+							// Diagnostic Tests
+							//require_once(WP_PLUGIN_DIR . "/" . basename(dirname(__FILE__)) . "/diagnostic.php");
+
+							// TODO: Add Diagnostic
+							?>
+					</div>
+
+				</div>
+			</div>
+		</div>
+
+		<?php
+		}
+
+
+	/*
+	 * Configuration
+	 */
+		static function Configuration() {
+
+		}
+
+	/*
+	 * Style
+	 */
+		static function Style() {
+
+			echo 'Method Variable called';
+
+
+
+		}
+
+
+
+	/*
+	 * Data: Gender
+	 */
+		static function DataGender() {
+
+			echo 'Method Variable called';
+
+
+
+		}
+
+
+
+	/*
+	 * Data: Profile Types
+	 */
+		static function DataProfileType() {
+
+			echo 'Method Variable called';
+
+
+
+		}
+
+
+	/*
+	 * Data: Custom Fields
+	 */
+		static function DataCustomFields() {
+
+			echo 'Method Variable called';
+
+
+
+		}
+
+
+
+
+	/*
+	 * Data: Media Types
+	 */
+		static function DataMediaType() {
+
+			echo 'Method Variable called';
+
+
+
+		}
+
+
+
+
+	/*
+	 * Data: Media Types
+	 */
+		static function DataMediaType() {
+
+			echo 'Method Variable called';
+
+			if( isset($_REQUEST['action']) && !empty($_REQUEST['action']) ) {
+				if($_REQUEST['action'] == 'douninstall') {
+					return RBAgency::Uninstall();
+				}
+			}
+
+		}
+
+
+}
+
+
+?>
