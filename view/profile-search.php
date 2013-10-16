c8ab18a74400c322154b6ef67fe4574a8cffbad5
robertholf <rob@bertholf.com>
2013-10-15 23:09:38 -1000

    Cleanup

--- view/profile-search.php
+++ view/profile-search.php
@@ -8,35 +8,34 @@ get_header();
 	echo "	<div id=\"content\" role=\"main\" class=\"transparent\">\n";
 	echo "		<div id=\"profile-search\">\n";
 
-		if ($_REQUEST["action"] == "search") {
-		echo "  <h1 class=\"entry-title\">". __("Search Results", rb_agency_TEXTDOMAIN) ."</h1>\n";
-		} else {
-		echo "  <h1 class=\"entry-title\">". __("Advanced Search", rb_agency_TEXTDOMAIN) ."</h1>\n";
-		}
+	if ($_REQUEST["action"] == "search") {
+	echo "			<h1 class=\"entry-title\">". __("Search Results", rb_agency_TEXTDOMAIN) ."</h1>\n";
+	} else {
+	echo "			<h1 class=\"entry-title\">". __("Advanced Search", rb_agency_TEXTDOMAIN) ."</h1>\n";
+	}
+
+	echo "			<div id=\"profile-search-results\">\n";
+	if ($_REQUEST["action"] == "search") {
 
 
+		// Process Form Submission & catch variables
+		$search_array = RBAgency_Profile::search_process();
+
+		// Return SQL string based on fields
+		$search_sql_query = RBAgency_Profile::search_generate_sqlwhere($search_array);
+
+		// Process Form Submission
+		$search_results = RBAgency_Profile::search_results($search_sql_query, 0);
+		$formatted = RBAgency_Profile::search_formatted($search_results);
 
 
-		echo "	<div id=\"profile-search-results\">\n";
-		if ($_REQUEST["action"] == "search") {
+	} else {
+	echo "				<strong>". __("No search chriteria selected, please initiate your search.", rb_agency_TEXTDOMAIN) ."</strong>";
+	}
+	echo "			</div><!-- #profile-search-results -->\n"; // #profile-search-results
+	echo "			<hr />";
 
-			// Process Form Submission
-			$search_array = RBAgency_Profile::search_process();
-
-			// Return Search
-			$search_sql_query = RBAgency_Profile::search_generate_sqlwhere($search_array);
-
-			// Process Form Submission
-			$search_results = RBAgency_Profile::search_results($search_sql_query, 0);
-			$formatted = RBAgency_Profile::search_formatted($search_results);
-
-		} else {
-			echo "<strong>". __("No search chriteria selected, please initiate your search.", rb_agency_TEXTDOMAIN) ."</strong>";
-		}
-		echo "</div><!-- #profile-search-results -->\n"; // #profile-search-results
-		echo "<hr />";
-
-		return RBAgency_Profile::search_form("", "", 0);
+	return RBAgency_Profile::search_form("", "", 0);
 
 	echo "		</div><!-- #profile-search -->\n"; // #profile-search
 	echo "	</div><!-- #content -->\n"; // #content
