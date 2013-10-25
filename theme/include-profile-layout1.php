57e65f175d0da7b97d3e21e6846a5e95e5daf94c
itexpertsindya <info@itexpertsindya.com>
2013-10-25 05:35:53 -0700

    Modified include-profile-layout1.php to display media types under profile page

--- theme/include-profile-layout1.php
+++ theme/include-profile-layout1.php
@@ -120,6 +120,18 @@ echo "					<ul>\n";
 									echo "<li class=\"item video demoreel\"><a href=\"http://www.youtube.com/watch?v=". $dataMedia['ProfileMediaURL'] ."\" title=\"". $dataMedia['ProfileMediaTitle'] ."\" target=\"_blank\"></a><a href=\"http://www.youtube.com/watch?v=". $dataMedia['ProfileMediaURL'] ."\" target=\"_blank\" class=\"rb_button\">Watch Demo Reel</a></li>\n";
 							  	}
 							}
+
+							// Other Media Type not the 
+							// default ones
+							$resultsMedia = mysql_query("SELECT * FROM ". table_agency_profile_media ." media WHERE ProfileID =  \"". $ProfileID ."\" 
+							                             AND ProfileMediaType NOT IN ('Image','Resume','CompCard','Comp Card','Headshot','VoiceDemo','Voice Demo','Video Slate','Video Monologue','Demo Reel')
+														 ");
+							$countMedia = mysql_num_rows($resultsMedia);
+							if ($countMedia > 0) {
+							  	while ($dataMedia = mysql_fetch_array($resultsMedia)) {
+				                    echo "<li class=\"item video demoreel\"><a href=\"". rb_agency_UPLOADDIR . $ProfileGallery ."/". $dataMedia['ProfileMediaURL'] ."\" class=\"rb_button\">".$dataMedia['ProfileMediaType']. "</a></li>\n";
+							  	}
+							}
 							//Contact Profile
 							if($rb_agency_option_showcontactpage==1){
 					    		echo "<div class=\"rel\"><strong>". __("Contact: ", rb_agency_TEXTDOMAIN). "<span class=\"divider\">:</span></strong> <a href=\"". get_bloginfo("wpurl") ."/profile/".$ProfileGallery	."/contact/\" class=\"rb_button\">Click Here</a></div>\n";
