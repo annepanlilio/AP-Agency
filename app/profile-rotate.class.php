431c472ad17b8e2d75aee8061c45f84fe961f951
Satya <prosatya@gmail.com>
2014-02-10 03:54:22 -0800

    Create Image Rotate class and remove imageRotate.php

---
+++ app/profile-rotate.class.php
@@ -0,0 +1,27 @@
+<?php
+
+class RBAgency_imageRotate{
+
+	
+	// File and rotation
+	// TODO: Need to test and implement not found any referance for testing
+	function imageRotate($name,$imgsrc){
+	
+		$directoryPath = $_SERVER['DOCUMENT_ROOT']."/wp-content/uploads/profile-media/" ; 
+		$filename = $directoryPath.$name."/"$imgsrc;
+		$degrees = 90;
+		// Load
+		$source = imagecreatefromjpeg($filename);
+		// Rotate
+		$rotate = imagerotate($source, $degrees, 0);
+		// Output
+		$fh=fopen($filename,'w');
+		imagejpeg($rotate,$filename);
+		fclose($fh);
+	}
+
+
+}
+
+
+?>
