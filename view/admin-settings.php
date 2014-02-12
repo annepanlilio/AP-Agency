d7b02ab20ee8ed1c8036638eeab011e853985f7e
Satya <prosatya@gmail.com>
2014-02-12 01:43:03 -0800

    Fixed Profile Layout issue

--- view/admin-settings.php
+++ view/admin-settings.php
@@ -423,7 +423,7 @@ elseif ($ConfigID == 1) {
 			$x=6;
 			while($x<=15) {
 				if (file_exists(rb_agency_BASEREL .'view/layout/'. sprintf("%02s", $x) .'/include-profile.php')) {
-				echo "       <option value=\"". $x ."\" ". selected($rb_agency_options_arr['rb_agency_option_layoutprofile'], 6,false) ."> ". __("Layout". $k, rb_agency_TEXTDOMAIN). $x ."</option>\n";
+				echo "       <option value=\"". $x ."\" ". selected($rb_agency_options_arr['rb_agency_option_layoutprofile'], $x,false) ."> ". __("Layout". $k, rb_agency_TEXTDOMAIN). $x ."</option>\n";
 				}
 				$x++;
 			}
