607f586868eb7d18d28b9256fd6bdf8ab33ef793
Phel <honeydove_mhar@yahoo.com>
2014-03-20 11:51:48 +0800

    Polished layout 8 video

--- view/layout/08/include-profile.php
+++ view/layout/08/include-profile.php
@@ -42,12 +42,12 @@ $rb_agency_options_arr = get_option('rb_agency_options');
 $order = $rb_agency_options_arr['rb_agency_option_galleryorder'];
 ?>
 
-<div id="rbprofile">
+<div id="rbprofile t">
 	<div id="rblayout-eight" class="rblayout">
 		<div class="rbcol-12 rbcolumn">
 			<div id="layout-head">
 				<?php echo " <h1>". $ProfileContactDisplay ."</h1>\n"; ?>
-				<a href="">Back to First Page</a>
+				<a href="">Back</a>
 			</div>
 		</div>
 		<div class="rbclear"></div>
@@ -76,9 +76,33 @@ $order = $rb_agency_options_arr['rb_agency_option_galleryorder'];
 			<ul id="profile-info">
 				<?php
 				// Insert Custom Fields
-				rb_agency_getNewProfileCustomFields($ProfileID, $ProfileGender, $LabelTag="em", $LabelSeparator=": ");
+				rb_agency_getNewProfileCustomFields($ProfileID, $ProfileGender, $LabelTag="em", $LabelSeparator=" ");
 				?>			
 			</ul>
+			<div id="videos">
+			<h4>Videos</h4>
+			<?php 
+			$queryMedia = "SELECT * FROM " . table_agency_profile_media . " WHERE ProfileID =  \"" . $ProfileID . "\" AND ProfileVideoType IN('youtube','vimeo')";
+			$resultsMedia =  $wpdb->get_results($wpdb->prepare($queryMedia),ARRAY_A);
+			$countMedia = $wpdb->num_rows;
+			foreach ($resultsMedia  as $dataMedia) {
+				if ($dataMedia['ProfileMediaType'] == "Demo Reel" || $dataMedia['ProfileMediaType'] == "Video Monologue" || $dataMedia['ProfileMediaType'] == "Video Slate") {
+					if($dataMedia['ProfileVideoType'] == "" || $dataMedia['ProfileVideoType'] == "youtube"){
+						$outVideoMedia .= "<div class=\"video-col\"><div class=\"video\"><a href=\"http://www.youtube.com/watch?v=" . $dataMedia['ProfileMediaURL'] . "\" target=\"_blank\">" .$dataMedia["ProfileMediaTitle"]."</a></div></div>\n";
+					}elseif($dataMedia['ProfileVideoType'] == "vimeo"){
+						$json = file_get_contents('http://vimeo.com/api/v2/video/'.$dataMedia['ProfileMediaURL'].'.json');
+						$data = json_decode($json,true);
+							$outVideoMedia .= "<div class=\"video-col\"><div class=\"video\"><a href=\"http://vimeo.com/" . $dataMedia['ProfileMediaURL'] . "\" target=\"_blank\">" .$dataMedia["ProfileMediaTitle"]."</a></div></div>\n";
+						}
+					} 
+				}
+				echo $outVideoMedia;
+			?>
+			</div>			
+			
+		</div>
+		<div class="rbcol-12 rbcolumn">
+			<a href="" title=""></a>
 		</div>
 	</div><!-- #rblayout-eight -->
 </div><!-- #profile -->
