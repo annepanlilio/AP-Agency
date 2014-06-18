228eeafd6c107bcac38a2684db6fc6dc928a310f
Champ Camba <info@champiewebfolio.com>
2014-06-19 02:27:11 +0800

    Remove white spaces

--- view/admin-search.php
+++ view/admin-search.php
@@ -67,12 +67,11 @@
 		if (isset($_POST["form_action"]) && $_POST["form_action"] == "search_profiles") {
 
 			echo "<div id=\"profile-search-results\">\n";
-
 				$search_array = RBAgency_Profile::search_process();
 				$search_sql_query = RBAgency_Profile::search_generate_sqlwhere($search_array);
-
+				
 				echo RBAgency_Profile::search_results($search_sql_query, 0);
-
+				
 			echo "</div><!-- #profile-search-results -->\n"; // #profile-search-results
 
 		}
