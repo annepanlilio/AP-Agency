b053448b658d441e6068e46776287a504635053f
Champ Camba <info@champiewebfolio.com>
2014-07-04 13:48:39 +0800

    Force download file

---
+++ ext/forcedownload.php
@@ -0,0 +1,21 @@
+<?php
+if(isset($_GET['file'])){
+    //Please give the Path like this
+    $file = $_GET['file'];
+
+    if (file_exists($file)) {
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename='.basename($file));
+        header('Content-Transfer-Encoding: binary');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($file));
+        ob_clean();
+        flush();
+        readfile($file);
+        exit;
+    }
+}
+?>
