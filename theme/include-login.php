d95035be84b82ddb6ced7751ae2b66f20e8ff254
Champ Camba <info@champiewebfolio.com>
2014-07-07 21:25:23 +0800

    Remove notices

--- theme/include-login.php
+++ theme/include-login.php
@@ -19,7 +19,7 @@ echo "	<div id=\"rbsignin-register\" class=\"rbinteract\">\n";
 			}
 
 echo "        <div id=\"rbsign-in\" class=\"inline-block\">\n";
-echo "          <h1>". __("Members Sign in", rb_agencyinteract_TEXTDOMAIN). "</h1>\n";
+echo "          <h1>". __("Members Sign in", rb_agency_interact_TEXTDOMAIN). "</h1>\n";
 
 	// Which Login Form should we send the user to?
 	if(function_exists('rb_agency_interact_menu')){
@@ -28,18 +28,18 @@ echo "          <h1>". __("Members Sign in", rb_agencyinteract_TEXTDOMAIN). "</h
 echo "          <form name=\"loginform\" id=\"login\" action=\"". $login_post_to ."\" method=\"post\">\n";
 echo " 			<input type=\"hidden\" name=\"redirect_to\" value=\"".network_site_url("/")."dashboard/\">\n";
 echo "            <div class=\"field-row\">\n";
-echo "              <label for=\"user-name\">". __("Username", rb_agencyinteract_TEXTDOMAIN). "</label><input type=\"text\" name=\"user-name\" value=\"". wp_specialchars( $_POST['user-name'], 1 ) ."\" id=\"user-name\" />\n";
+echo "              <label for=\"user-name\">". __("Username", rb_agency_interact_TEXTDOMAIN). "</label><input type=\"text\" name=\"user-name\" value=\"". wp_specialchars( isset($_POST['user-name'])?$_POST['user-name']:"", 1 ) ."\" id=\"user-name\" />\n";
 echo "            </div>\n";
 echo "            <div class=\"field-row\">\n";
-echo "              <label for=\"password\">". __("Password", rb_agencyinteract_TEXTDOMAIN). "</label><input type=\"password\" name=\"password\" value=\"\" id=\"password\" /> <a href=\"". get_bloginfo('wpurl') ."/wp-login.php?action=lostpassword\">". __("forgot password", rb_agencyinteract_TEXTDOMAIN). "?</a>\n";
+echo "              <label for=\"password\">". __("Password", rb_agency_interact_TEXTDOMAIN). "</label><input type=\"password\" name=\"password\" value=\"\" id=\"password\" /> <a href=\"". get_bloginfo('wpurl') ."/wp-login.php?action=lostpassword\">". __("forgot password", rb_agency_interact_TEXTDOMAIN). "?</a>\n";
 echo "            </div>\n";
 echo "            <div class=\"field-row\">\n";
-echo "              <input type=\"checkbox\" name=\"remember-me\" value=\"forever\" /> ". __("Keep me signed in", rb_agencyinteract_TEXTDOMAIN). "\n";
+echo "              <input type=\"checkbox\" name=\"remember-me\" value=\"forever\" /> ". __("Keep me signed in", rb_agency_interact_TEXTDOMAIN). "\n";
 echo "            </div>\n";
 echo "            <div class=\"field-row submit-row\">\n";
 echo "              <input type=\"hidden\" name=\"action\" value=\"log-in\" />\n";
 echo "                <input name=\"lastviewed\" value=\"".$profileviewed."\" type=\"hidden\" />\n";
-echo "              <input type=\"submit\" value=\"". __("Sign In", rb_agencyinteract_TEXTDOMAIN). "\" /><br />\n";
+echo "              <input type=\"submit\" value=\"". __("Sign In", rb_agency_interact_TEXTDOMAIN). "\" /><br />\n";
 echo "            </div>\n";
 echo "          </form>\n";
 	} else {
