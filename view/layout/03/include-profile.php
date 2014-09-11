00e1420011adacadfe58ee8997d3aef8547dfbba
Champ Camba <champ.kazban25@gmail.com>
2014-09-11 13:10:23 +0800

    Update layout03 styles

--- view/layout/03/include-profile.php
+++ view/layout/03/include-profile.php
@@ -242,7 +242,16 @@ echo " 							<div class=\"tab-panel\">\n";
 									if ($countMedia > 0) {
 									  		foreach($resultsMedia as $dataMedia ){
 											$profileVideoEmbed = $dataMedia['ProfileMediaURL'];
-											echo"<div class=\"video slate rbcol-4 rbcolumn\"><div class=\"video-container\"><object width=\"350\" height=\"220\"><param name=\"movie\" value=\"http://www.youtube.com/v/". $profileVideoEmbed ."?fs=1&amp;hl=en_US&rel=0&showsearch=0\"></param><param name=\"allowFullScreen\" value=\"true\"></param><param name=\"allowscriptaccess\" value=\"always\"></param><embed src=\"http://www.youtube.com/v/". $profileVideoEmbed ."?fs=1&amp;hl=en_US\" type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\" width=\"350\" height=\"220\"></embed></object></div></div>\n";
+											//echo"<div class=\"video slate rbcol-4 rbcolumn\"><div class=\"video-container\"><object width=\"350\" height=\"220\"><param name=\"movie\" value=\"". $profileVideoEmbed ."?fs=1&amp;hl=en_US&rel=0&showsearch=0\"></param><param name=\"allowFullScreen\" value=\"true\"></param><param name=\"allowscriptaccess\" value=\"always\"></param><embed src=\"". $profileVideoEmbed ."?fs=1&amp;hl=en_US\" type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\" width=\"350\" height=\"220\"></embed></object></div></div>\n";
+									  		echo"<div class=\"video monologue rbcol-2 rbcolumn\">\n";
+									  		echo "<div class=\"video-container\">\n";
+									  		echo "<a href=\"".$profileVideoEmbed."\"  target=\"_blank\" rel=\"nofollow\">\n";
+									  		echo rb_agency_get_videothumbnail($profileVideoEmbed );
+									  		echo "<span class=\"videotitle\">".ucfirst($dataMedia['ProfileVideoType'])." Video</span>";
+									  		echo "</a>\n";
+									  		echo "</div>\n";
+									  		echo "</div>\n";
+									  	
 									  	}
 									}
 
@@ -253,7 +262,18 @@ echo " 							<div class=\"tab-panel\">\n";
 									if ($countMedia > 0) {
 									  	foreach($resultsMedia as $dataMedia ){
 											$profileVideoEmbed = $dataMedia['ProfileMediaURL'];
-											echo"<div class=\"video monologue rbcol-4 rbcolumn\"><div class=\"video-container\"><object width=\"350\" height=\"220\"><param name=\"movie\" value=\"http://www.youtube.com/v/". $profileVideoEmbed ."?fs=1&amp;hl=en_US&rel=0&showsearch=0\"></param><param name=\"allowFullScreen\" value=\"true\"></param><param name=\"allowscriptaccess\" value=\"always\"></param><embed src=\"http://www.youtube.com/v/". $profileVideoEmbed ."?fs=1&amp;hl=en_US\" type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\" width=\"350\" height=\"220\"></embed></object></div></div>\n";
+											//echo"<div class=\"video monologue rbcol-2 rbcolumn\"><div class=\"video-container\"><object width=\"350\" height=\"220\"><param name=\"movie\" value=\"". $profileVideoEmbed ."?fs=1&amp;hl=en_US&rel=0&showsearch=0\"></param><param name=\"allowFullScreen\" value=\"true\"></param><param name=\"allowscriptaccess\" value=\"always\"></param><embed src=\"". $profileVideoEmbed ."?fs=1&amp;hl=en_US\" type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\" width=\"350\" height=\"220\"></embed></object></div></div>\n";
+									  		echo"<div class=\"video monologue rbcol-2 rbcolumn\">\n";
+									  		echo "<div class=\"video-container\">\n";
+									  		echo "<a href=\"".$profileVideoEmbed."\"  target=\"_blank\" rel=\"nofollow\">\n";
+									  		echo rb_agency_get_videothumbnail($profileVideoEmbed );
+									  		echo "<span class=\"videotitle\">".ucfirst($dataMedia['ProfileVideoType'])." Video</span>";
+									  		echo "</a>\n";
+									  		echo "</div>\n";
+									  		echo "</div>\n";
+									  	
+									  	
+												 
 									  	}
 									}
 
@@ -264,7 +284,17 @@ echo " 							<div class=\"tab-panel\">\n";
 									if ($countMedia > 0) {
 									  	foreach($resultsMedia as $dataMedia ){
 											$profileVideoEmbed = $dataMedia['ProfileMediaURL'];
-											echo"<div class=\"video demoreel rbcol-4 rbcolumn\"><div class=\"video-container\"><object width=\"350\" height=\"220\"><param name=\"movie\" value=\"http://www.youtube.com/v/". $profileVideoEmbed ."?fs=1&amp;hl=en_US&rel=0&showsearch=0\"></param><param name=\"allowFullScreen\" value=\"true\"></param><param name=\"allowscriptaccess\" value=\"always\"></param><embed src=\"http://www.youtube.com/v/". $profileVideoEmbed ."?fs=1&amp;hl=en_US\" type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\" width=\"350\" height=\"220\"></embed></object></div></div>\n";
+											//echo"<div class=\"video demoreel rbcol-2 rbcolumn\"><div class=\"video-container\"><object width=\"350\" height=\"220\"><param name=\"movie\" value=\"". $profileVideoEmbed ."?fs=1&amp;hl=en_US&rel=0&showsearch=0\"></param><param name=\"allowFullScreen\" value=\"true\"></param><param name=\"allowscriptaccess\" value=\"always\"></param><embed src=\"". $profileVideoEmbed ."?fs=1&amp;hl=en_US\" type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\" width=\"350\" height=\"220\"></embed></object></div></div>\n";
+									  		echo"<div class=\"video monologue rbcol-2 rbcolumn\">\n";
+									  		echo "<div class=\"video-container\">\n";
+									  		echo "<a href=\"".$profileVideoEmbed."\" target=\"_blank\" rel=\"nofollow\">\n";
+									  		echo rb_agency_get_videothumbnail($profileVideoEmbed );
+									  		echo "<span class=\"videotitle\">".ucfirst($dataMedia['ProfileVideoType'])." Video</span>";
+									  		echo "</a>\n";
+									  		echo "</div>\n";
+									  		echo "</div>\n";
+									  	
+									  	
 									  	}
 									}
 echo " 							</div>\n"; // .tab-panel								
