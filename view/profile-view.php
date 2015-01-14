d44aa6aff201be6a1051447f737e34e3dc233f91
Champ Camba <champ.kazban25@gmail.com>
2015-01-14 15:33:33 +0800

    Fixed profile privacy

--- view/profile-view.php
+++ view/profile-view.php
@@ -166,7 +166,11 @@
 				(is_user_logged_in() && current_user_can( 'edit_posts' )) ||
 
 				//  Must be logged as "Client" to view model list and profile information
-				($rb_agency_option_privacy == 3 && is_user_logged_in() && is_client_profiletype() || ($ProfileUserLinked == $CurrentUser)  ) ) {
+				($rb_agency_option_privacy == 3 && is_user_logged_in() && is_client_profiletype() /*|| ($ProfileUserLinked == $CurrentUser)*/  ) ||
+
+				//  Model list public. Must be logged to view profile information
+				($rb_agency_option_privacy == 1 && is_user_logged_in())
+				) {
 
 				// Ok, but whats the status of the profile?
 				if ( ($ProfileIsActive == 1) || ($ProfileUserLinked == $CurrentUser) || current_user_can('level_10') ) {
