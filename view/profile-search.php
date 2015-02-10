911aa8e4f2194a339e85779bf5cc9f82cbbde387
Champ Camba <champ.kazban25@gmail.com>
2015-02-11 00:18:01 +0800

    Fixed broken link to login

--- view/profile-search.php
+++ view/profile-search.php
@@ -111,7 +111,7 @@ $rb_agency_option_formhide_advancedsearch_button = isset($rb_agency_options_arr[
 					if ( class_exists("RBAgencyCasting") ) {
 						echo "<h2>Page restricted. Only Admin & Casting Agent can view this page. Please <a href=\"".get_bloginfo("url")."/casting-login/\">login or register</a>.</h2>";
 					}else{
-						echo "<h2>Page restricted. Please <a href=\"".get_bloginfo("url")."/profile-login/\">login</a> or <a href=\"".get_bloginfo("url")."/profile-register/\">register</a>.</h2>";
+						echo "<h2>Page restricted. Please <a href=\"".get_bloginfo("url")."/profile-login/\">login</a> or <a href=\"".get_bloginfo("url")."/profile-login/\">register</a>.</h2>";
 					}
 					echo "	</div><!-- .restricted -->\n";
 				}
