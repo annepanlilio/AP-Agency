3503166e33783ae0a63575d21e39b5d46048b373
Champ Camba <champ.kazban25@gmail.com>
2015-02-11 00:55:07 +0800

    Fixed Display Name

--- lib/RBAgency-Profile.php
+++ lib/RBAgency-Profile.php
@@ -2106,7 +2106,7 @@ class RBAgency_Profile {
 				} elseif ($rb_agency_option_profilenaming == 1) {
 					$ProfileContactDisplay = $ProfileContactNameFirst . " ". substr($ProfileContactNameLast, 0, 1);
 				} elseif ($rb_agency_option_profilenaming == 2) {
-					$ProfileContactDisplay = $ProfileContactNameFirst;
+					$ProfileContactDisplay = $dataList["ProfileContactDisplay"];
 				} elseif ($rb_agency_option_profilenaming == 3) {
 					$ProfileContactDisplay = "ID ". $ProfileID;
 				} elseif ($rb_agency_option_profilenaming == 4) {
