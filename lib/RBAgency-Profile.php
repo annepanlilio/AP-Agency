8e87048e58cea4f484a46b03f4ce7851d38fc53e
Champ Camba <champ.kazban25@gmail.com>
2015-02-20 17:51:05 +0800

    Fixed advanced search link

--- lib/RBAgency-Profile.php
+++ lib/RBAgency-Profile.php
@@ -642,7 +642,11 @@ class RBAgency_Profile {
 					if ($type == 1) {
 					echo "				<input type=\"button\" onclick=\"window.location.href='". get_bloginfo("wpurl") ."/search-basic/'\" value=\"". __("Go to Basic Search", RBAGENCY_TEXTDOMAIN) . "\"/>\n";
 					} elseif($rb_agency_option_formhide_advancedsearch_button != 1) {
-					echo "				<input type=\"button\" onclick=\"window.location.href='". get_bloginfo("wpurl") ."/search-advanced/'\" value=\"". __("Go to Advanced Search", RBAGENCY_TEXTDOMAIN) . "\"/>\n";
+						 if(is_admin()){
+						 	echo "				<input type=\"button\" onclick=\"window.location.href='".admin_url("admin.php?page=rb_agency_search")."'\" value=\"". __("Go to Advanced Search", RBAGENCY_TEXTDOMAIN) . "\"/>\n";
+						 }else{
+							echo "				<input type=\"button\" onclick=\"window.location.href='". get_bloginfo("wpurl") ."/search-advanced/'\" value=\"". __("Go to Advanced Search", RBAGENCY_TEXTDOMAIN) . "\"/>\n";
+						 }
 					}
 				echo "				</div>\n";
 				global $user_ID, $wpdb;
