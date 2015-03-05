481911cf4f88432cced259a9cf52f97a137bbdfd
Champ Camba <champ.kazban25@gmail.com>
2015-03-05 21:49:19 +0800

    Fixed print compatible with sort

--- assets/js/rb-printProfiles.js
+++ assets/js/rb-printProfiles.js
@@ -2,20 +2,22 @@
  * Print profiles
 */
 jQuery(document).ready(function(){
+		
+
+	jQuery(".link-profile-print").click(function(){
+		jQuery(".rb-print").remove();
 		jQuery.each(jQuery(".rbprofile-list"),function(i,d){
 			if(i%9==0){
 					if(i>1){
-									jQuery("<div class=\"print-clear\"></div><div class='rb-print-footer'><span>"+rb_agency.name+"</span></div><div class=\"print-clear\"></div><!--rb-printProfile.js-->").insertBefore(jQuery(this));
+									jQuery("<div class='rb-print'><div class=\"print-clear\"></div><div class='rb-print-footer'><span>"+rb_agency.name+"</span></div><div class=\"print-clear\"></div></div><!--rb-printProfile.js-->").insertBefore(jQuery(this));
 					
 					}
-							jQuery("<div class=\"print-clear\"></div><div class='rb-print-header'><img src=\""+rb_agency.logo+"\"/><span style=\"float:right;\">"+rb_agency.name+"</span></div><div class=\"print-clear\"></div><!--rb-printProfile.js-->").insertBefore(jQuery(this));
+							jQuery("<div class='rb-print'><div class=\"print-clear\"></div><div class='rb-print-header'><img src=\""+rb_agency.logo+"\"/><span style=\"float:right;\">"+rb_agency.name+"</span></div><div class=\"print-clear\"></div></div><!--rb-printProfile.js-->").insertBefore(jQuery(this));
 				
 				
 			}
 			
 		});
-
-	jQuery(".link-profile-print").click(function(){
 			window.print();
 	});
 });
