deecadc88c39e44c0204c7a6ccab460d760543ff
Champ Camba <champ.kazban25@gmail.com>
2015-03-17 21:18:16 +0800

    Adjust profile listing

--- assets/js/rb-printProfiles.js
+++ assets/js/rb-printProfiles.js
@@ -7,7 +7,7 @@ jQuery(document).ready(function(){
 	jQuery(".link-profile-print").click(function(){
 		jQuery(".rb-print").remove();
 		jQuery.each(jQuery(".rbprofile-list"),function(i,d){
-			if(i%5==0){
+			if(i%6==0){
 					if(i>1){
 									jQuery("<div class='rb-print'><div class=\"print-clear\"></div><div class='rb-print-footer'><span>"+rb_agency.name+"</span></div><div class=\"print-clear\"></div></div><!--rb-printProfile.js-->").insertBefore(jQuery(this));
 					
