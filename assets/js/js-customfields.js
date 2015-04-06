a692a540ce1843ba2a11baa66c96b8a0f55223c0
phelm <honeydove_mhar@yahoo.com>
2015-04-06 14:28:11 -0700

    Admin Settings > Edit Custom Fields: Fix Delete field option not working

--- assets/js/js-customfields.js
+++ assets/js/js-customfields.js
@@ -251,13 +251,13 @@ jQuery(document).ready(function(){
      }
 	
 		jQuery("#addmoreoption_1").on('click',function(){
-			jQuery("#editfield_add_more_options_12").append("<li class=\"rbfield rbtext rbsingle\"><label>Option:</label><div class=\"option\"><input type=\"text\" name=\"option[]\"><a href='javascript:;' class='del_opt' title='Delete Option' style='color:red; text-decoration:none'>&nbsp;[ - ]</a></div></li>");
+			jQuery("#editfield_add_more_options_12").append("<li class=\"rbfield rbtext rbsingle\"><label>Option:</label><div class=\"option\"><input type=\"text\" name=\"option[]\"><a href='javascript:;' class='del_opt' onclick='del_opt(jQuery(this));' title='Delete Option' style='color:red; text-decoration:none'>&nbsp;[ - ]</a></div></li>");
 		});
 		jQuery("#addmoreoption_2").on('click',function(){
-			jQuery("#editfield_add_more_options_2").append("<li class=\"rbfield rbtext rbsingle\"><label>Option:</label><div class=\"option\"><input type=\"text\" name=\"option2[]\"><a href='javascript:;' class='del_opt' title='Delete Option' style='color:red; text-decoration:none'>&nbsp;[ - ]</a></div></li>");
+			jQuery("#editfield_add_more_options_2").append("<li class=\"rbfield rbtext rbsingle\"><label>Option:</label><div class=\"option\"><input type=\"text\" name=\"option2[]\"><a href='javascript:;' class='del_opt' onclick='del_opt(jQuery(this));' title='Delete Option' style='color:red; text-decoration:none'>&nbsp;[ - ]</a></div></li>");
 		}); 
 		jQuery("#addmoreoption_12").on('click',function(){
-			jQuery("#editfield_add_more_options_12").append("<li class=\"rbfield rbtext rbsingle\"><label>Option:</label><div class=\"option\"><input type=\"text\" name=\"option[]\"><a href='javascript:;' class='del_opt' title='Delete Option' style='color:red; text-decoration:none'>&nbsp;[ - ]</a></div></li>");
+			jQuery("#editfield_add_more_options_12").append("<li class=\"rbfield rbtext rbsingle\"><label>Option:</label><div class=\"option\"><input type=\"text\" name=\"option[]\"><a href='javascript:;' class='del_opt' onclick='del_opt(jQuery(this));' title='Delete Option' style='color:red; text-decoration:none'>&nbsp;[ - ]</a></div></li>");
 		});
 
 		jQuery("a.del_opt").on('click',function(){
