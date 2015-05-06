e6189c100fc7921822bdd002d2b6c671e3e3a1be
Champ Camba <champ.kazban25@gmail.com>
2015-05-07 03:10:20 +0800

    Fixed casting cart view

--- view/profile-admincart.php
+++ view/profile-admincart.php
@@ -12,24 +12,28 @@ global $wpdb;
 
 		// Get Profile
 		 $SearchMuxHash = get_query_var('target');
-
 		if (isset($SearchMuxHash)) {
-
+			
 			// Get Identifier
 			$_SESSION['SearchMuxHash'] = $SearchMuxHash;
-
+			//$wpdb->query("ALTER TABLE  ". table_agency_searchsaved." ADD INDEX (`SearchProfileID`)");
+			//$wpdb->query("ALTER TABLE  ". table_agency_searchsaved_mux." ADD INDEX (`SearchID`)");
 			// Get Casting Cart by Identifier
 			$query = "SELECT search.SearchTitle, search.SearchProfileID, search.SearchOptions, searchsent.SearchMuxHash, searchsent.SearchMuxCustomThumbnail FROM ". table_agency_searchsaved ." search LEFT JOIN ". table_agency_searchsaved_mux ." searchsent ON search.SearchID = searchsent.SearchID WHERE searchsent.SearchMuxHash = \"". $SearchMuxHash ."\"";
 			$data = $wpdb->get_row($query,ARRAY_A) or die ( __("Error, query failed", RBAGENCY_TEXTDOMAIN ));
 			$count =  $wpdb->num_rows;
+			/*$wpdb->show_errors();
+			$wpdb->print_error();
+			*/
 			// Get Casting Cart ID
 			$castingcart_id = implode(",",array_unique(array_filter(explode(",",$data['SearchProfileID']))));
+			
 			$arr = (array)unserialize($data["SearchMuxCustomThumbnail"]);
 			$_SESSION["profilephotos_view"] = is_array($arr[0])?array_filter(array_unique($arr[0])):"";
 
 			$search_array = array("perpage" => 9999, "include" => $castingcart_id);
-			$search_sql_query = RBAgency_Profile::search_generate_sqlwhere(array_filter(array_unique($search_array)));
-
+			//$search_sql_query = RBAgency_Profile::search_generate_sqlwhere(array_filter(array_unique($search_array)));
+			$search_sql_query['standard'] = " profile.ProfileID IN(".(!empty($castingcart_id)?$castingcart_id:0).") ";
 			// Process Form Submission
 			echo $search_results = RBAgency_Profile::search_results($search_sql_query, 3);
 			
