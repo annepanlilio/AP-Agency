119e8124090878c66174c3ab94c63bc2fa4dc309
Champ Camba <champ.kazban25@gmail.com>
2015-05-07 09:33:55 +0800

    Fixed export profiles

--- view/admin-reports.php
+++ view/admin-reports.php
@@ -1250,7 +1250,7 @@ elseif ($ConfigID == 81)
 			$to = $total_profiles->total_profiles;
 		}
 		
-		echo "<input  required type=\"radio\" name=\"export-profile\" value=\"".($from - 1)."-".$to."\">Export Profiles(".($from)."-".$to.")<br/>";
+		echo "<input  required type=\"radio\" name=\"export-profile\" value=\"".((($from - 1) == 0)?1:$to+1-(100))."-".((($from - 1) == 0)?100:$count)."\">Export Profiles(".($from)."-".$to.")<br/>";
 		
 		$loop_count = (($a==1)?$count:$loop_count + $count);
 		
