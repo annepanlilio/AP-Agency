d490b4a13a54a1a786a5f462d0ca40ae32707875
charliemac24 <charlieanchetamacaraeg@gmail.com>
2015-06-02 09:26:32 +0800

    Fixed on missing file BFI_Thumb.php

--- functions.php
+++ functions.php
@@ -715,7 +715,7 @@
 				'crop'=>true,
 				'width'=>200
 			);
-			$image_src = bfi_thumb( $image_path, $bfi_params );
+			@$image_src = bfi_thumb( $image_path, $bfi_params );
 			//$images ."<img  class=\"roll\" src=\"".RBAGENCY_PLUGIN_URL."/ext/timthumb.php?src={PHOTO_PATH}". $img_src ."&w=200&q=30\" alt='' style='width:148px'   />\n";
 			$images ."<img  class=\"roll\" src=\"". $image_src ."\" alt='' style='width:148px'   />\n";
 		}
