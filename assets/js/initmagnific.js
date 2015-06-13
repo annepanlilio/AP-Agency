0ef948123eea9911bcc31b745d23f575f01ed435
phelm <honeydove_mhar@yahoo.com>
2015-06-13 16:01:00 +0800

    Add magnific pop up and voiceover resources

---
+++ assets/js/initmagnific.js
@@ -0,0 +1,5 @@
+jQuery('.profile-popup').magnificPopup({
+	type:'inline',
+	closeBtnInside:false,
+  	midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+});
