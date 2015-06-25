1a29de7763b6094c667399f643cd902b3c1f82c4
charliemac24 <charlieanchetamacaraeg@gmail.com>
2015-06-25 11:38:24 +0800

    Fixed casting job exporter

--- rb-agency.php
+++ rb-agency.php
@@ -6,9 +6,9 @@ Plugin URI: http://rbplugin.com/wordpress/model-talent-agency-software/
 Description: With this plugin you can easily manage models profiles and information.
 Author: Rob Bertholf
 Author URI: http://rob.bertholf.com/
-Version: 2.4.6
+Version: 2.4.5
 */
-$RBAGENCY_VERSION = "2.4.6";
+$RBAGENCY_VERSION = "2.4.5";
 /* If you modify the plugin set the following to TRUE */
 $RBAGENCY_CUSTOM = FALSE;
 /*
