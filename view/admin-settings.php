f70f6a55e7fcde1b5514aa9785c0ae36c2620976
phelm <honeydove_mhar@yahoo.com>
2015-10-30 09:58:06 +0800

    Update layout13 label

--- view/admin-settings.php
+++ view/admin-settings.php
@@ -741,7 +741,7 @@ elseif ($ConfigID == 1) {
 		echo "       <option value=\"11\" ". selected(isset($rb_agency_options_arr['rb_agency_option_layoutprofile'])?$rb_agency_options_arr['rb_agency_option_layoutprofile']:0, 11,false) ."> ". __("Layout 11 - Profile View with Thumbnails, Primary Image, Print Photos & Print Polaroids", RBAGENCY_TEXTDOMAIN) ."</option>\n";
 		echo "       <option value=\"12\" ". selected(isset($rb_agency_options_arr['rb_agency_option_layoutprofile'])?$rb_agency_options_arr['rb_agency_option_layoutprofile']:0, 12,false) ."> ". __("Layout 12 - Profile View with Thumbnails, Primary Image, Print Photos & Print Polaroids", RBAGENCY_TEXTDOMAIN) ."</option>\n";
 		echo "       <option value=\"13\" ". selected(isset($rb_agency_options_arr['rb_agency_option_layoutprofile'])?$rb_agency_options_arr['rb_agency_option_layoutprofile']:0, 13,false) ."> ". __("Layout 13 - Primary Image with scrolling thumbnails and thumbnail gallery", RBAGENCY_TEXTDOMAIN) ."</option>\n";
-			$x=13;
+			$x=14;
 			while($x<=15) {
 				if (file_exists(RBAGENCY_PLUGIN_DIR .'view/layout/'. sprintf("%02s", $x) .'/include-profile.php')) {
 				echo "       <option value=\"". $x ."\" ". selected(isset($rb_agency_options_arr['rb_agency_option_layoutprofile'])?$rb_agency_options_arr['rb_agency_option_layoutprofile']:0, $x,false) ."> ". __("Layout ", RBAGENCY_TEXTDOMAIN). $x ."</option>\n";
