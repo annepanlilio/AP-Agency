80d5de5612808c23681169b4d74e63fe4a1ee51b
charliemac24 <charlieanchetamacaraeg@gmail.com>
2016-02-19 10:08:13 -0800

    Fixed errors in profile view

--- functions.php
+++ functions.php
@@ -1872,7 +1872,7 @@
 	 * ======================== Get Custom Fields ===============
 	 * @Returns Custom Fields
 	/*/
-	function rb_agency_getProfileCustomFields($ProfileID, $ProfileGender, $table=false, $title_to_exclude, $label_tag="strong", $value_tag="span", $echo = true, $is_print = false) {
+	function rb_agency_getProfileCustomFields($ProfileID, $ProfileGender, $table=false, $title_to_exclude = array(), $label_tag="strong", $value_tag="span", $echo = true, $is_print = false) {
 
 		global $wpdb;
 		$rb_agency_options_arr = get_option('rb_agency_options');
