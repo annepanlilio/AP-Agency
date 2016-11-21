72183b673139963b6e5df0e92d3380ca5a547322
Charlie <charlieanchetamacaraeg@gmail.com>
2016-11-21 15:20:59 +0800

    Allow ut8f character in sending emails

--- app/casting.class.php
+++ app/casting.class.php
@@ -818,7 +818,7 @@ class RBAgency_Casting {
 							$castingInfo .="</div><!-- #searchsaved-emailsent -->";
 			// Mail it
 			$headers[]  = 'MIME-Version: 1.0'. "\r\n";
-			$headers[] = 'Content-type: text/html; charset=iso-8859-1'. "\r\n";
+			$headers[] = 'Content-type: text/html; charset=UTF-8'. "\r\n";
 
 			if(!empty($SearchMuxFromName)){
 				$rb_agency_value_agencyname = $SearchMuxFromName;
